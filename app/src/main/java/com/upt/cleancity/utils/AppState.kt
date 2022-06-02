608127f374c4b67446e2e310951575d362329c3c
BlagaD <dan.blaga@ness.com>
2022-06-02 11:11:31 +0300

    Changed API URL

--- app/src/main/java/com/upt/cleancity/utils/AppState.kt
+++ app/src/main/java/com/upt/cleancity/utils/AppState.kt
@@ -5,7 +5,7 @@ import com.upt.cleancity.model.Token
 import com.upt.cleancity.model.User
 
 object AppState {
-    const val API_BASE_URL = "https://10.0.2.2:7222/"
+    const val API_BASE_URL = "https://cleancityadministration20220602012919.azurewebsites.net/"
     var loggedInUser = User()
     var currentToken = Token()
     var storageReference = FirebaseStorage.getInstance().reference
